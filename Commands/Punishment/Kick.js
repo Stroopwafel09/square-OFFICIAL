73c90bbb04df8e9ce6c73e95cb01b55dc9e83e2d
Stroopwafel09 <107180056+Stroopwafel09@users.noreply.github.com>
2024-10-03 17:38:49 +0200

    Update Kick.js

--- Commands/Punishment/Kick.js
+++ Commands/Punishment/Kick.js
@@ -34,7 +34,7 @@ class Kick extends Command {
 
         await Target.kick();
 
-        return await this.Bot.send({ content: `${Target} has been successfully kicked from the server. ✅`, ephemeral: false });
+        return await this.Bot.send({ content: `${Target} has been successfully kicked from the server. ✅`});
 
     }
 
