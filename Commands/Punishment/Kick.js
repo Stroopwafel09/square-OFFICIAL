2d0a8e88161644ae73308f9a7a95005a20054ac6
Stroopwafel09 <107180056+Stroopwafel09@users.noreply.github.com>
2024-10-03 18:01:46 +0200

    Update Kick.js

--- Commands/Punishment/Kick.js
+++ Commands/Punishment/Kick.js
@@ -29,12 +29,12 @@ class Kick extends Command {
     async run(interaction, guild, member, args) {
 
         const Target = guild.members.cache.get(args[0].value);
-        if(!Target) return await this.Bot.send('User not found!');
-        if(!Target.kickable) return await this.Bot.send('❌ You do not have a permission to kick this user!');
+        if(!Target) return await this.Bot.send(interaction, 'User not found!');
+        if(!Target.kickable) return await this.Bot.send(interaction, '❌ You do not have a permission to kick this user!');
 
         await Target.kick();
 
-        return await this.Bot.send(' ${Target} has been successfully kicked from the server. ✅');
+        return await this.Bot.send(interaction, ` ${Target} has been successfully kicked from the server. ✅`);
 
     }
 
