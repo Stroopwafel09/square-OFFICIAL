edc26ae51d0d5c7bc03edb1bd27739bcb04e449d
Stroopwafel09 <107180056+Stroopwafel09@users.noreply.github.com>
2024-10-04 17:42:29 +0200

    Update unban.js

--- Commands/Punishment/unban.js
+++ Commands/Punishment/unban.js
@@ -7,12 +7,12 @@ class Unban extends Command {
             enabled: true,
             required_perm: "BAN_MEMBERS",
             usages: ["unban"],
-            description: "Unban members from the guild.",
+            description: "Unban members from guild.",
             category: "Punishment",
             options: [{
                 name: "user_id",
                 description: "Enter the ID of the user to unban.",
-                type: 3, // 3 is type STRING (for user ID)
+                type: 3, // 3 is type STRING for user ID
                 required: true
             }]
         });
@@ -23,17 +23,24 @@ class Unban extends Command {
     }
 
     async run(interaction, guild, member, args) {
-        const userId = args[0].value;
+        console.log("Command executed.");
+
+        const userId = args[0].value; // Verwacht een string voor de gebruikers-ID
+        console.log("User ID to unban:", userId);
+
+        // Check of de uitvoerende lid de permissie heeft om te bannen
+        console.log("Member permissions:", member.permissions.toArray());
+        if (!member.permissions.has("BAN_MEMBERS")) {
+            return await this.Bot.send(interaction, `❌ You do not have permission to unban members!`);
+        }
 
         try {
             await guild.members.unban(userId);
-            return await interaction.reply({ content: `User with ID ${userId} has been successfully unbanned. ✅` });
+            console.log("User unbanned.");
+            return await this.Bot.send(interaction, `User with ID ${userId} has been successfully unbanned from the server. ✅`);
         } catch (error) {
-            if (error.code === 10026) {
-                return await interaction.reply({ content: `❌ User with ID ${userId} is not banned.`, ephemeral: true });
-            } else {
-                return await interaction.reply({ content: `❌ An error occurred while trying to unban the user. Please check my permissions or the user ID.`, ephemeral: true });
-            }
+            console.error("Error unbanning user:", error);
+            return await this.Bot.send(interaction, `❌ An error occurred while trying to unban the user. They might not be banned or the ID is incorrect.`);
         }
     }
 }
