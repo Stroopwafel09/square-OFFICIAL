46e8c1ef743fc9608d94b9981dcdbfb262065a87
Stroopwafel09 <107180056+Stroopwafel09@users.noreply.github.com>
2024-10-04 15:43:15 +0200

    Update Kick.js

--- Commands/Punishment/Kick.js
+++ Commands/Punishment/Kick.js
@@ -3,7 +3,6 @@ const Command = require("../../Utils/Command.js");
 class Kick extends Command {
     
     constructor(Bot) {
-
         super(Bot, {
             enabled: true,
             required_perm: "KICK_MEMBERS",
@@ -17,27 +16,34 @@ class Kick extends Command {
                 required: true
             }]
         });
-
     }
 
     load() {
-            
         return;
-
     }
 
     async run(interaction, guild, member, args) {
-
         const Target = guild.members.cache.get(args[0].value);
-        if(!Target) return await this.Bot.send(interaction, 'User not found!');
-        if(!Target.kickable) return await this.Bot.send(interaction, '❌ You do not have a permission to kick this user!');
 
+        // Check if the target user is found
+        if (!Target) {
+            return await this.Bot.send(interaction, '❌ User not found in this guild!');
+        }
+
+        // Check if the member executing the command has permission to kick
+        if (!member.permissions.has("KICK_MEMBERS")) {
+            return await this.Bot.send(interaction, '❌ You do not have permission to kick members!');
+        }
+
+        // Check if the target user can be kicked
+        if (!Target.kickable) {
+            return await this.Bot.send(interaction, '❌ I cannot kick this user. They might have a higher role or I do not have permission to kick them!');
+        }
+
+        // Proceed to kick the user
         await Target.kick();
-
-        return await this.Bot.send(interaction, ` ${Target} has been successfully kicked from the server. ✅`);
-
+        return await this.Bot.send(interaction, `${Target} has been successfully kicked from the server. ✅`);
     }
-
 }
 
-module.exports = Kick; 
+module.exports = Kick;
