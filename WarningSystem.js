6c4e18d08a7371c6968ea4a38d82ebd0441e62b5
Stroopwafel09 <107180056+Stroopwafel09@users.noreply.github.com>
2024-10-04 17:52:09 +0200

    Create WarningSystem.js

---
+++ WarningSystem.js
@@ -0,0 +1,31 @@
+// WarningSystem.js
+
+class WarningSystem {
+    constructor() {
+        this.warnings = new Map(); // Store warnings in a Map
+    }
+
+    addWarning(userId, warning) {
+        if (!this.warnings.has(userId)) {
+            this.warnings.set(userId, []);
+        }
+        this.warnings.get(userId).push(warning);
+    }
+
+    getWarnings(userId) {
+        return this.warnings.get(userId) || [];
+    }
+
+    removeWarning(userId, index) {
+        if (this.warnings.has(userId)) {
+            const userWarnings = this.warnings.get(userId);
+            if (index >= 0 && index < userWarnings.length) {
+                userWarnings.splice(index, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+module.exports = WarningSystem;
